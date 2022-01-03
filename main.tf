afbd70d57acd5807201171075ea7ca9c55cab6fd
suryatejayalla <88100093+suryatejayalla@users.noreply.github.com>
2022-01-03 09:47:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_network_interface" "ni" {
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  instance_type = ${var.instance_type}
   key_name      = "ohio"
   depends_on    =  [aws_vpc.vpc1, aws_subnet.subnet1]
 
