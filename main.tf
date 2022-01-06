2d448dacd6516713ffcac2f4fbd3c3baa718a293
suryateja <suryatejayalla@gmail.com>
2022-01-06 08:36:14 +0530

    c

--- main.tf
+++ main.tf
@@ -75,18 +75,18 @@ resource "aws_instance" "ec2" {
   instance_type = "${var.instance_type}"
   key_name      = "ohio"
   depends_on    =  [aws_vpc.vpc1, aws_subnet.subnet1]
+
+  network_interface {
+    network_interface_id = aws_network_interface.ni.id
+    device_index         = 0
+  }
+  
+    credit_specification {
+      cpu_credits = "unlimited"
+
+  }
+
+    tags = {
+      Name = "coupa"
+  }
 }
-
-network_interface {
-  network_interface_id = aws_network_interface.ni.id
-  device_index         = 0
-  }
-
-credit_specification {
-  cpu_credits = "unlimited"
-
-  }
-
-tags = {
-  Name = "coupa"
-  }
